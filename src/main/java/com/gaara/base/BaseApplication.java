1eb1d0c7b5da457a44bfd292d25dbd880cc10934
gaara <759499745@qq.com>
2022-07-03 18:28:55 +0800

    启动信息

--- src/main/java/com/gaara/base/BaseApplication.java
+++ src/main/java/com/gaara/base/BaseApplication.java
@@ -1,15 +1,33 @@
 package com.gaara.base;
 
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.util.Date;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.gaara.base.mapper")
+@Slf4j
 public class BaseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BaseApplication.class, args);
+        SpringApplication application = new SpringApplication(BaseApplication.class);
+        Environment environment = application.run(args).getEnvironment();
+        log.info("\n--------------------------------base-------------------------------------\n\t" +
+                        "Application '{}' is running! \n\t" +
+                        "Now is 【{}】\n\t"+
+                        "Url is : 【'http://localhost:{}'】\n\t"+
+                        "Mysql url : 【{}】\n\t"
+                ,
+                environment.getProperty("spring.profiles.active") == null ? "OnLine":environment.getProperty("spring.profiles.active"),
+                JSONObject.toJSONStringWithDateFormat(new Date(),"yyyy-MM-dd hh:mm:ss"),
+                environment.getProperty("server.port"),
+                environment.getProperty("spring.datasource.url")
+        );
+        System.out.print("---------------------------------------------------------------------------\n\t");
     }
-
 }
